d34292c2275804599f0c3ef16a8cd0fa9734eb3a
Akshay Sahu <akki5233@gmail.com>
2021-04-16 23:56:20 +0530

    Added quote icon as well

--- reviews_card/src/Review.js
+++ reviews_card/src/Review.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import data from './data';
 ////// NEED TO ADD STYLING AND REACT ICONS /////
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import { FaQuoteRight } from 'react-icons/fa';
 const Review = ()=> {
 
     const [index, setIndex] = useState(0);
@@ -48,19 +49,24 @@ const Review = ()=> {
         <div className="review">
                 <div className="img-container ">
                     <img src={image} alt={name} className="person-img" />
+                    <span className='quote-icon'>
+                        <FaQuoteRight />
+                    </span>
                 </div>
 
-                <div className="author">
-                    <h4>{name}</h4>
-                </div>
-                <div className="job">
+                <h4 className="author">
+                    {name}
+                </h4>
+                <p className="job">
                     {job}
-                </div>
-                <div className="info">
+                </p>
+                <p className="info">
                     {text}
+                </p>
+                <div className="btn-container">
+                    <button className="prev-btn" onClick={prevBtn}> <AiOutlineLeft/></button>
+                    <button className="next-btn" onClick={nextBtn}><AiOutlineRight/></button>
                 </div>
-                <button className="prev-btn" onClick={prevBtn}>{AiOutlineLeft}</button>
-                <button className="next-btn" onClick={nextBtn}>{AiOutlineRight}</button>
                 <div><button className="random-btn" onClick={randomBtn}>Surprise me</button></div>
         </div>
     </article>
